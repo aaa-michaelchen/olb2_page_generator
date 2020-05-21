c81bc5df112026cf7b57109252529597448016a0
Michael Chen <chen.lichieh@aaa-calif.com>
2020-05-20 22:53:05 -0700

    add base and state presentation

--- program.py
+++ program.py
@@ -37,11 +37,21 @@ def save(content, dest, data):
 
 
 def handle_add_node_files(content):
+    print('HANDLING NODE FILES')
     base_node_destination_path = 'baseNode'
     state_node_destination_path = 'stateNode'
 
     load_and_save(content, base_node_destination_path, 'base_node_template')
     load_and_save(content, state_node_destination_path, 'state_node_template')
+
+
+def handle_add_presentation_files(content):
+    print('HANDLING PRESENTATION FILES')
+    base_presentation_destination_path = 'basePresentation'
+    state_presentation_destination_path = 'statePresentation'
+
+    load_and_save(content, base_presentation_destination_path, 'base_presentation_template')
+    load_and_save(content, state_presentation_destination_path, 'state_presentation_template')
 
 
 def load_and_save(content, destination_path, template):
@@ -55,8 +65,11 @@ def main():
 
     node_name = input('node name: ')
     flow = input('which flow: ')
+
     node_content = get_node_content(node_name, flow)
+
     handle_add_node_files(node_content)
+    handle_add_presentation_files(node_content)
 
     print('done')
 
